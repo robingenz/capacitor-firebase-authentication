17c0f7d668b5c0212fafa65a0887a44de6f3bf38
Robin Genz <mail@robingenz.dev>
2021-08-12 12:34:30 +0200

    ios

--- ios/Plugin/Handlers/OAuthProviderHandler.swift
+++ ios/Plugin/Handlers/OAuthProviderHandler.swift
@@ -13,8 +13,24 @@ class OAuthProviderHandler: NSObject {
 
     func signIn(call: CAPPluginCall, providerId: String) {
         self.provider = OAuthProvider(providerID: providerId)
+        self.applySignInConfig(call: call, provider: provider!);
         DispatchQueue.main.async {
             self.startSignInFlow()
+        }
+    }
+    
+    private func applySignInConfig(call: CAPPluginCall, provider: OAuthProvider) {
+        let customParameters = call.getArray("customParameters", JSObject.self) ?? []
+        for (_,customParameter) in customParameters.enumerated() {
+            let key = customParameter["key"] as? String
+            let value = customParameter["value"] as? String
+            guard let key = key else {
+                continue
+            }
+            guard let value = value else {
+                continue
+            }
+            provider.customParameters?[key] = value
         }
     }
 
