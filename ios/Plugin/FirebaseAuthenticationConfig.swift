4b2bd8425eb8a7438f499dbde9bb9211f92cf2dd
Robin Genz <mail@robingenz.dev>
2021-08-22 14:26:04 +0200

    wip

--- ios/Plugin/FirebaseAuthenticationConfig.swift
+++ ios/Plugin/FirebaseAuthenticationConfig.swift
@@ -1,10 +1,10 @@
 public struct FirebaseAuthenticationConfig {
     var skipNativeAuth = false
     var providers = ["apple.com",
-        "facebook.com",
-        "github.com",
-        "google.com",
-        "microsoft.com",
-        "twitter.com",
-        "yahoo.com"]
+                     "facebook.com",
+                     "github.com",
+                     "google.com",
+                     "microsoft.com",
+                     "twitter.com",
+                     "yahoo.com"]
 }
